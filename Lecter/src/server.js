592379dc56ad1d7d658a8e017fe3e475e0094072
rahdnus <rahdnus119@gmail.com>
2023-09-07 22:09:08 +0530

    Init Commit

---
+++ Lecter/src/server.js
@@ -0,0 +1,14 @@
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
